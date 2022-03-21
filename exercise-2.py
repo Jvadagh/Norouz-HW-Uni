9da2da70be3c4b584e421e9b6599ee232f7ba60c
V3il3DOraCl3 <javadaaghajanloo@gmail.com>
2022-03-21 09:19:36 +0330

    upload exercise-2.py

--- exercise-2.py
+++ exercise-2.py
@@ -1,1 +1,17 @@
-#o_O
+def GCD(first, second):  # GCD : greatest common divisor
+    if first > second:
+        BMM = second
+    elif second >= first:
+        BMM = first
+    while BMM >= 1:
+        if first % BMM == 0 and second % BMM == 0:
+            print("G.C.D is ", BMM)
+            break
+        else:
+            BMM -= 1
+            if first % BMM == 0 and second % BMM == 0:
+                return print("B.M.M is", BMM)
+if __name__ == '__main__':
+    FirstNum=int(input("insert first number : "))
+    SecondNum=int(input("insert second number : "))
+    GCD(FirstNum,SecondNum)
